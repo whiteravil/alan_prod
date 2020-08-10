aa8578155ad46f5b15131464cfcd2a40d5510d25
whiteravil <whiteravil@yandex.ru>
2020-08-11 01:13:44 +0300

    statistics

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -53,15 +53,48 @@ $(function() {
 		toggleBy: 'roles-toggle'
 	});
 
+	let editRoles = new KTOffcanvas('edit-roles', {
+		overlay: true,
+		baseClass: 'offcanvas',
+		placement: 'right',
+	});
+
+	let viewRoles = new KTOffcanvas('view-roles', {
+		overlay: true,
+		baseClass: 'offcanvas',
+		placement: 'right',
+	});
+
 	$('.open-edit').on('click', function(e) {
 		e.preventDefault();
 		editGoals.show();
 	});
 
-	$('.canvas-close').on('click', function(e) {
+	$('.open-edit-role').on('click', function(e) {
 		e.preventDefault();
-		editGoals.hide();
+		editRoles.show();
 	});
 
+	$('.open-view-role').on('click', function(e) {
+		e.preventDefault();
+		viewRoles.show();
+	});
+
+	$('.canvas-close').on('click', function(e) {
+		e.preventDefault();
+		editGoals.classOverlay !== undefined ? editGoals.hide() : null;
+		roles.classOverlay !== undefined ? roles.hide() : null;
+		editRoles.classOverlay !== undefined ? editRoles.hide() : null;
+		viewRoles.classOverlay !== undefined ? viewRoles.hide() : null;
+	});
+
+	$('.daterange-picker').daterangepicker({
+		buttonClasses: 'btn',
+		applyClass: 'btn-primary',
+		cancelClass: 'btn-secondary',
+		locale: {
+			format: 'DD.MM.YYYY'
+		}
+	});
 
 });
